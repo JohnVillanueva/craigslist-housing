d10bb129b8327d8d91dbccfe046fedaa038bcfa0
JohnVillanueva <johnmvillan@gmail.com>
2021-05-03 23:30:46 -0700

    Added Boxplots and Mapbox

--- dashboard/dash_board.py
+++ dashboard/dash_board.py
@@ -2,16 +2,18 @@ import sqlite3
 from sqlalchemy import create_engine, text, select
 from sqlalchemy.orm import sessionmaker
 
-
-
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objs as go
 
 import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
+
+MAPBOX_TOKEN = 'pk.eyJ1Ijoiam9obnZpbGxhbnVldmEiLCJhIjoiY2tvM29ybG14MGljZDMxcGRiNzIwN3E5OSJ9.mHxEvurXk2fTpHRS7pEbWA'
 
 app = dash.Dash(__name__)
 
@@ -24,7 +26,7 @@ app = dash.Dash(__name__)
 #     dbdata = connection.execute(text("SELECT * FROM apartment"))
 #     df = pd.DataFrame(dbdata)
 
-df = pd.read_csv("sfaptsscraper/sfaptsscraper/sfapts.csv")
+df = pd.read_csv("../sfaptsscraper/sfaptsscraper/sfapts.csv")
 df['neighborhood'] = df['neighborhood'].fillna('San Francisco')
 
 # Aggregated Price Data
@@ -38,7 +40,8 @@ dff = df.groupby('neighborhood').agg(
             'max'
         ]}
     ).sort_values(
-        by=('price','count'), ascending = False
+        by=('price','count'),
+        ascending = False
     )
 
 dff.columns = dff.columns.droplevel()
@@ -55,6 +58,14 @@ app.layout = html.Div([
         options = [{'label': neighborhood, 'value': neighborhood} for neighborhood in df.neighborhood.unique()],
         value = dff.neighborhood[:3]
     ),
+
+    html.H2('Geo Scatterplot of Selected Neighborhoods'),
+
+    dcc.Graph(id='geoscatterplot-graph'),
+
+    html.H2('Price Boxplots by Neighborhood'),
+
+    dcc.Graph(id='neighborhood-boxplots'),
 
     html.H2('Price Histogram for Selected Neighborhoods'),
 
@@ -94,6 +105,30 @@ def neighborhood_price_stats(neighborhood):
     filtered_dff = dff.loc[dff.neighborhood.isin(neighborhood),]
     return filtered_dff.to_dict('records')
 
+@app.callback(
+    Output('geoscatterplot-graph', 'figure'),
+    Input('neighborhood-options', 'value')
+)
+def mapbox_scatter(neighborhood):
+    filtered_df = df.loc[df.neighborhood.isin(neighborhood),]
+    px.set_mapbox_access_token(MAPBOX_TOKEN)
+    figure = px.scatter_mapbox(
+        filtered_df, lat='latitude', lon='longitude', color = 'price', size = 'bedrooms', hover_name='name',
+        color_continuous_scale = px.colors.diverging.Earth, size_max = 10, zoom=10)
+    return figure
+
+@app.callback(
+    Output('neighborhood-boxplots', 'figure'),
+    Input('neighborhood-options', 'value')
+)
+def neighborhood_boxplots(neighborhood):
+    filtered_df = df.loc[df.neighborhood.isin(neighborhood),]
+    figure = px.box(filtered_df, x='neighborhood', y='price', color='neighborhood')
+    return figure
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+#------------------------------------------------------------------------------
+
+
